4854e9c2673be7a47a686e8796d02a5d4a9cf933
Natalie R <tatysia@gmail.com>
2023-02-03 00:41:12 +0300

    Задача 1 о количестве введенных положительных чисел

---
+++ Ex1_MNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿// Ввести с клавиатуры M чисел. Вывести количество чисел больше 0.
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число M :");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] matr)
+{
+    for (int i = 0; i < matr.Length; i++)
+    {
+        Console.Write($"Число {i + 1} = ");
+        matr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void CountPositive(int[] matr)
+{
+    int count = 0;
+    for (int i = 0; i < matr.Length; i++)
+    {
+        if (matr[i] > 0) count++;
+    }
+    Console.WriteLine($"Количество чисел больше 0 = {count}");
+}
+
+int number = GetNumber();
+int[] massive = CreateArray(number);
+Console.WriteLine();
+FillArray(massive);
+Console.WriteLine();
+CountPositive(massive);
